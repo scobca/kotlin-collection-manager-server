5630aace91fac80a222ca4856a325e5380334d5b
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-03-25 02:03:32 +0300

    feat(kafka): linked 2 microservices <3

--- file-service/src/main/kotlin/org/itmo/fileservice/FileServiceApplication.kt
+++ file-service/src/main/kotlin/org/itmo/fileservice/FileServiceApplication.kt
@@ -1,10 +1,37 @@
 package org.itmo.fileservice
 
+import org.itmo.fileservice.config.dto.KafkaSystemMessage
+import org.itmo.fileservice.config.enums.SystemServices
+import org.itmo.fileservice.config.enums.SystemThemes
+import org.itmo.fileservice.utils.ReactiveKafkaConsumer
+import org.itmo.fileservice.utils.ReactiveKafkaProducer
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import reactor.core.scheduler.Schedulers
 
 @SpringBootApplication
-class FileServiceApplication
+class FileServiceApplication(
+    private val producer: ReactiveKafkaProducer,
+    private val consumer: ReactiveKafkaConsumer
+) : CommandLineRunner {
+
+    override fun run(vararg args: String?) {
+        val message = KafkaSystemMessage(SystemThemes.SERVICE_STARTED, SystemServices.FILE_SERVICE, null)
+        producer.sendEvent("system", message)
+            .doFinally { println("Message sent") }
+            .subscribeOn(Schedulers.boundedElastic())
+            .subscribe()
+
+        consumer.listenRSSEvent()
+            .doOnNext { it -> println(it.message) }
+            .subscribeOn(Schedulers.boundedElastic())
+            .subscribe(
+                { },
+                { error -> println("Error: $error") }
+            )
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<FileServiceApplication>(*args)
--- invoker-service/src/main/kotlin/org/itmo/invokerservice/InvokerServiceApplication.kt
+++ invoker-service/src/main/kotlin/org/itmo/invokerservice/InvokerServiceApplication.kt
@@ -1,10 +1,42 @@
 package org.itmo.invokerservice
 
+import org.itmo.invokerservice.config.dto.KafkaRequireSystemSynchronizationMessage
+import org.itmo.invokerservice.config.enums.SystemThemes
+import org.itmo.invokerservice.utils.ReactiveKafkaConsumer
+import org.itmo.invokerservice.utils.ReactiveKafkaProducer
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import reactor.core.scheduler.Schedulers
 
 @SpringBootApplication
-class InvokerServiceApplication
+class InvokerServiceApplication(
+    private val producer: ReactiveKafkaProducer,
+    private val consumer: ReactiveKafkaConsumer
+) : CommandLineRunner {
+    override fun run(vararg args: String?) {
+        consumer.listenSystemEvent()
+            .doOnEach { it ->
+                println(it)
+                if (it.get()?.theme == SystemThemes.SERVICE_STARTED) {
+                    producer.sendEvent(
+                        "require-system-synchronization",
+                        KafkaRequireSystemSynchronizationMessage("Required")
+                    )
+                        .subscribeOn(Schedulers.boundedElastic())
+                        .subscribe()
+                    println("Required")
+                }
+                println("mapped")
+
+            }
+            .subscribeOn(Schedulers.boundedElastic())
+            .subscribe(
+                { println("Event approved") },
+                { error -> println("Error: $error") }
+            )
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<InvokerServiceApplication>(*args)
