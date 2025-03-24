7046b1973f67b40b1fa7b5f4b258e9f75edd8166
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-03-24 16:43:55 +0300

    feat(kafka): updated Kafka topics

--- collection-service/src/main/kotlin/org/itmo/collectionservice/config/KafkaConfig.kt
+++ collection-service/src/main/kotlin/org/itmo/collectionservice/config/KafkaConfig.kt
@@ -10,64 +10,64 @@ class KafkaConfig {
     @Bean
     fun systemTopic(): NewTopic {
         return TopicBuilder.name("system")
-            .partitions(3)
-            .replicas(3)
+            .partitions(6)
+            .replicas(2)
             .build()
     }
 
     @Bean
     fun requireSystemSynchronizationTopic(): NewTopic {
         return TopicBuilder.name("require-system-synchronization")
-            .partitions(3)
-            .replicas(3)
+            .partitions(6)
+            .replicas(2)
             .build()
     }
 
     @Bean
     fun commandsSynchronizationTopic(): NewTopic {
         return TopicBuilder.name("commands-synchronization")
-            .partitions(3)
-            .replicas(3)
+            .partitions(6)
+            .replicas(2)
             .build()
     }
 
     @Bean
     fun collectionSynchronizationTopic(): NewTopic {
         return TopicBuilder.name("collection-synchronization")
-            .partitions(3)
-            .replicas(3)
+            .partitions(6)
+            .replicas(2)
             .build()
     }
 
     @Bean
     fun errorTopic(): NewTopic {
         return TopicBuilder.name("error")
-            .partitions(3)
-            .replicas(3)
+            .partitions(6)
+            .replicas(2)
             .build()
     }
 
     @Bean
     fun commandRequestTopic(): NewTopic {
         return TopicBuilder.name("command-request")
-            .partitions(3)
-            .replicas(3)
+            .partitions(6)
+            .replicas(2)
             .build()
     }
 
     @Bean
     fun commandResponseTopic(): NewTopic {
         return TopicBuilder.name("command-response")
-            .partitions(3)
-            .replicas(3)
+            .partitions(6)
+            .replicas(2)
             .build()
     }
 
     @Bean
     fun collectionChangesTopic(): NewTopic {
         return TopicBuilder.name("collection-changes")
-            .partitions(3)
-            .replicas(3)
+            .partitions(6)
+            .replicas(2)
             .build()
     }
 }
