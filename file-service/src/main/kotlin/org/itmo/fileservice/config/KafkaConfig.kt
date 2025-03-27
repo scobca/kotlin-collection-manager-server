5806ff675f2515d81e1f0d67a5f6be79b9317822
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-03-28 01:42:08 +0300

    feat(kafka): created kafka topics

---
+++ file-service/src/main/kotlin/org/itmo/fileservice/config/KafkaConfig.kt
@@ -0,0 +1,28 @@
+package org.itmo.fileservice.config
+
+import org.apache.kafka.clients.admin.NewTopic
+import org.itmo.fileservice.kafka.enums.KafkaEvents
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.TopicBuilder
+
+@Configuration
+class KafkaConfig {
+    @Bean
+    fun systemTopic(): NewTopic {
+        return TopicBuilder
+            .name(KafkaEvents.SYSTEM.name)
+            .partitions(3)
+            .replicas(1)
+            .build()
+    }
+
+    @Bean
+    fun collectionChanges(): NewTopic {
+        return TopicBuilder
+            .name(KafkaEvents.COLLECTION_UPDATE.name)
+            .partitions(3)
+            .replicas(1)
+            .build()
+    }
+}
