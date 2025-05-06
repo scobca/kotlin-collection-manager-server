0661a9c4d235c35aa075ceeac65512e2a728179d
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-05-07 00:38:11 +0300

    feat(utils): created string to flat converter

---
+++ collection-service/src/main/kotlin/org/itmo/collectionservice/utils/StringToFlatParser.kt
@@ -0,0 +1,28 @@
+package org.itmo.collectionservice.utils
+
+import org.itmo.collectionservice.collection.items.Coordinates
+import org.itmo.collectionservice.collection.items.Flat
+import org.itmo.collectionservice.collection.items.Furnish
+import org.itmo.collectionservice.collection.items.House
+
+object StringToFlatParser {
+    fun parseToFlat(input: String): Flat {
+        // [1,name,1,123,212,12,10000,true,little,house1,2000,12]
+
+        val data = input.replace("[", "").replace("]", "").split(",")
+
+        var flat = Flat()
+            .setId(data[0].toLong())
+            .setName(data[1])
+            .setCoordinates(Coordinates(data[2].toLong(), data[3].toFloat()))
+            .setArea(data[4].toLong())
+            .setNumberOfRooms(data[5].toLong())
+            .setPrice(data[6].toLong())
+            .setBalcony(data[7].toBoolean())
+            .setFurnish(Furnish.valueOf(data[8].uppercase().trim()))
+            .setHouse(House(data[9], data[10].toInt(), data[11].toLong()))
+
+        return flat
+    }
+
+}
