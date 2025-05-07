7732aef43550530bb367fc5333ec4086489fc5ea
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-05-07 16:11:52 +0300

    fix(tcp): returned server answer

--- invoker-service/src/main/kotlin/org/itmo/invokerservice/utils/TcpServerFactory.kt
+++ invoker-service/src/main/kotlin/org/itmo/invokerservice/utils/TcpServerFactory.kt
@@ -25,7 +25,7 @@ class TcpServerFactory(private val invokerService: InvokerService) {
 
                 val message = input.readLine()
                 val response = invokerService.handleCommand(message)
-
+                output.println(response)
 
                 socket.close()
             }
