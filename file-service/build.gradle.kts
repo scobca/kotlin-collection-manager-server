f6a250c4520c5b16f1272f4f0b2952a15d9a226a
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-05-20 01:30:16 +0300

    build(deps): installed plugins for web security

--- file-service/build.gradle.kts
+++ file-service/build.gradle.kts
@@ -29,9 +29,11 @@ dependencies {
     implementation("io.github.cdimascio:dotenv-java:2.2.0")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-security")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
 
     // Kafka
     implementation("org.springframework.kafka:spring-kafka")
