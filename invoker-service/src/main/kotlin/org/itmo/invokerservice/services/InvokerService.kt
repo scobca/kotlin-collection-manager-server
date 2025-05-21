7ede708c05eb4b3d3771a34a84a1b1a3a8b3ef16
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-05-21 08:46:05 +0300

    feat(api): updated invoker service

--- invoker-service/src/main/kotlin/org/itmo/invokerservice/services/InvokerService.kt
+++ invoker-service/src/main/kotlin/org/itmo/invokerservice/services/InvokerService.kt
@@ -1,6 +1,7 @@
 package org.itmo.invokerservice.services
 
 import org.itmo.invokerservice.storages.CommandsStorage
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBody
@@ -8,14 +9,24 @@ import org.springframework.web.reactive.function.client.awaitBody
 @Service
 class InvokerService(
     commandsStorage: CommandsStorage,
-    private val webClient: WebClient,
+    @Qualifier("collectionService") private val webClient: WebClient,
+    private val authService: AuthService,
 ) {
     private val commandsList = commandsStorage.getAllCommands()
+    private val authCommands = listOf<String>("login", "register", "refresh")
 
     suspend fun handleCommand(message: String): Any? {
         val line = message.trim().split(" ")
         val command = line[0].trim()
         val args = message.split(" ").drop(1)
+
+        if (authCommands.contains(command)) {
+            when (command) {
+                "login" -> {return authService.login(args)}
+                "register" -> {return authService.register(args)}
+                "refresh" -> {return authService.refreshTokens(args[0])}
+            }
+        }
 
         if (command == "help") {
             return commandsList
