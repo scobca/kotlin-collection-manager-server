b4e6240d6caeccc43428006d2faf4e37682847c8
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-05-21 07:48:47 +0300

    feat(api): create method for jwt extracting

--- collection-service/src/main/kotlin/org/itmo/collectionservice/utils/StringToFlatParser.kt
+++ collection-service/src/main/kotlin/org/itmo/collectionservice/utils/StringToFlatParser.kt
@@ -1,27 +1,35 @@
 package org.itmo.collectionservice.utils
 
-import org.itmo.collectionservice.collection.items.Coordinates
-import org.itmo.collectionservice.collection.items.Flat
+import org.itmo.collectionservice.api.dto.collection.CoordinatesDto
+import org.itmo.collectionservice.api.dto.collection.GetFlatDto
+import org.itmo.collectionservice.api.dto.collection.HouseDto
 import org.itmo.collectionservice.collection.items.Furnish
-import org.itmo.collectionservice.collection.items.House
 
 object StringToFlatParser {
-    fun parseToFlat(input: String): Flat {
+    fun parseToFlat(input: String): GetFlatDto {
 
         val data = input.replace("[", "").replace("]", "").split(",")
 
-        var flat = Flat()
-            .setId(data[0].toLong())
-            .setName(data[1])
-            .setCoordinates(Coordinates(data[2].toLong(), data[3].toFloat()))
-            .setArea(data[4].toLong())
-            .setNumberOfRooms(data[5].toLong())
-            .setPrice(data[6].toLong())
-            .setBalcony(data[7].toBoolean())
-            .setFurnish(Furnish.valueOf(data[8].uppercase().trim()))
-            .setHouse(House(data[9], data[10].toInt(), data[11].toLong()))
-
+        var flat = GetFlatDto(
+            id = data[0].toLong(),
+            name = data[1],
+            coordinates = CoordinatesDto(x = data[2].toLong(), y = data[3].toFloat()),
+            area = data[4].toLong(),
+            numberOfRooms = data[5].toLong(),
+            price = data[6].toLong(),
+            balcony = data[7].toBoolean(),
+            furnish = Furnish.valueOf(data[8].uppercase().trim()),
+            house = HouseDto(name = data[9], year = data[10].toInt(), numberOfFloors = data[11].toLong()),
+            user = null,
+        )
         return flat
+    }
+
+    fun extractJwt(input: String): String {
+        val data = input.replace("[", "").replace("]", "").split(",")
+        if (data.size == 13) return data.last()
+
+        return ""
     }
 
 }
